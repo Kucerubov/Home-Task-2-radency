a038fb29c96373286b5f15b674f4745169a82079
MrNeahka <kucerubov365@gmail.com>
2023-08-04 16:20:19 +0300

    final

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NoteAndCategoryTable from "./component/NoteAndCategoryTable";
+
+function App() {
+  return(
+      <div>
+        <NoteAndCategoryTable/>
+      </div>
+  );
+}
+
+export default App;
