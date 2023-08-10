21d90c948344f2ae8d0cdf4a0eadeabb39544894
MrNeahka <kucerubov365@gmail.com>
2023-08-10 15:40:40 +0300

    final

---
+++ my-app/src/component/styles/NoteAndCategoryTableStyle.ts
@@ -0,0 +1,10 @@
+export const buttonStylesBetweenTables = {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    margin: '10px'
+}
+
+export const buttonShowNotes = {
+    marginRight: '10px'
+}
